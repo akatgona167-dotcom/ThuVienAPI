1ff30ac4fec79fb9ee01104eeb2ad39979c3773c
Thứ Hết Tiền <akatgona167@gmail.com>
2026-09-24 14:46:39 +0700

    Phần 2: Xây dựng domain model class dùng EF Core – CodeFirs

--- obj/Debug/net10.0/WebAPI.AssemblyInfo.cs
+++ obj/Debug/net10.0/WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f901d47f3b160420dcf174694315fe49a4e1e8a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10c6fe2ba02a8d776a65d267d82b0749d962fa1c")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
